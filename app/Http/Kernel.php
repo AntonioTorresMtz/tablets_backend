f70509e61bc88063b7b303272956cf2795a1acc3
= <=>
2026-01-01 08:16:52 -0600

    Se modifico un archivo de los CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,9 +43,9 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'cors' => [\App\Http\Middleware\Cors::class],
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,            
         ],
+        'cors' => [\App\Http\Middleware\Cors::class],
     ];
 
     /**
