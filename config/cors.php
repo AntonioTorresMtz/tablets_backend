f10d8ac86105e4f98386e60c8698caf75c3b8f25
= <=>
2026-01-01 10:27:32 -0600

    Modificacion de CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost'],
+    'allowed_origins' => ['http://localhost',
+        'http://localhost:8080',
+        'http://127.0.0.1',
+        'http://127.0.0.1:8080',],
 
     'allowed_origins_patterns' => [],
 
